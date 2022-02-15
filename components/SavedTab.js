0b73edf914587f1afdd9e3de896dce6cdb71b76f
AmantinaR <amantinarossi@gmail.com>
2022-02-14 20:36:21 -0800

    creating screens

---
+++ components/SavedTab.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default function SavedTab({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Saved Tab</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
