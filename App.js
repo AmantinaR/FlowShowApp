b49d4ee3aecb222f8eccfbfc1780dfadc84fac90
AmantinaR <amantinarossi@gmail.com>
2022-02-17 19:21:19 -0800

    landing page and assets

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Text, View, StyleSheet, Button, SafeAreaView, TextInput } from 'react-native';
+import { Text, View, StyleSheet, Button, SafeAreaView, TextInput, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
--- components/Screens/LandingScreen.js
+++ components/Screens/LandingScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Text, View, StyleSheet, Button, SafeAreaView, TextInput } from 'react-native';
+import { Text, View, StyleSheet, Button, SafeAreaView, TextInput, Image, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -10,11 +10,20 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 export default function LandingScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Landing Screen</Text>
+      <Image source={require('../../assets/landing-blob.png')} style={styles.blob}/>
+      <Image source={require("../../assets/logo-large.png")} style={styles.logo}/>
       <StatusBar style="auto" />
-      <Button title="Search for a Bathroom" onPress={() => navigation.navigate('Tabs', { screen: 'Search' })}/>
-      <Button title="Report a Bathroom" onPress={() => navigation.navigate('Tabs', { screen: 'Report' })}/>
-      <Button title="Saved Bathrooms" onPress={() => navigation.navigate('Tabs', { screen: 'Saved' })}/>
+      <View style={styles.buttonsBox}>
+        <Pressable onPress={() => navigation.navigate('Tabs', { screen: 'Search' })}>
+          <Image source={require('../../assets/find-bathroom.png')} style={styles.button}/>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate('Tabs', { screen: 'Report' })}>
+          <Image source={require('../../assets/report-bathroom.png')} style={styles.button}/>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate('Tabs', { screen: 'Saved' })}>
+          <Image source={require('../../assets/saved-bathrooms.png')} style={styles.button}/>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
@@ -26,4 +35,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    position: 'absolute',
+    width: 102.65,
+    height: 148,
+    left: 146,
+    top: 249,
+  },
+  blob: {
+    position: 'absolute',
+    //width: 545,
+    //height: 406,
+    top: 352,
+  },
+  buttonsBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 0,
+    position: 'absolute',
+    width: 238,
+    height: 184,
+    left: 76,
+    top: 439,
+    //borderWidth: 1,
+    //borderColor: 'black'
+  },
+  button: {
+    marginTop: 10,
+    marginBottom: 0,
+  }
 });
