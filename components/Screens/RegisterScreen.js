79b5de7402efb2c10ac2d02935904b1a30053f1d
AmaraO416 <78100470+AmaraO416@users.noreply.github.com>
2022-02-25 11:34:17 -0800

    Update RegisterScreen.js
    Styled register page

--- components/Screens/RegisterScreen.js
+++ components/Screens/RegisterScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Text, View, StyleSheet, Button, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Button, SafeAreaView, TextInput, TouchableOpacity, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,17 +8,23 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 export default function RegisterScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Register Screen</Text>
+      <Image source={require("../../assets/logo-large.png")} style={styles.logo}/>
+      <Text style = {styles.register_header}>Register</Text>
       <StatusBar style="auto" />
       <TextInput style={styles.textBox} placeholder='username' autoCorrect={false}/>
       <TextInput style={styles.textBox} placeholder='email' autoCorrect={false}/>
       <TextInput style={styles.textBox} placeholder='password' secureTextEntry={true}/>
       <TouchableOpacity onPress={() => navigation.navigate('Landing')}>
-        <View>
-          <Text> Register</Text>
+      <View>
+        <Text style = {styles.button}>Register</Text>
         </View>
       </TouchableOpacity>
-
+      <View style = {styles.row}>
+      <Text style = {styles.already_member}>  Already a member?  </Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style = {styles.login}> Login</Text>
+      </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -30,12 +36,60 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textBox: {
-    borderWidth: 1,
-    borderColor: 'black',
-    height: 40,
-    padding: 10,
-    margin: 12,
-    width: 200
-  }
+logo: {
+  position: 'absolute',
+  top: 90
+},
+register_header: {
+  fontWeight: 'bold',
+  fontSize: 20,
+  top: 20,
+},
+textBox: {
+  borderWidth: 1,
+  borderColor: 'black',
+  height: 50,
+  padding: 10,
+  margin: 12,
+  width: 230,
+  top: 40
+},
+row: {
+  flexDirection: "row",
+  left: -75,
+  
+},
+already_member:{
+  top: 50
+},
+login: {
+  fontSize: 18,
+  top: 45,
+  color: '#ff8984',
+  textDecorationLine: 'underline'
+},
+button: {
+  color: '#ffffff',
+  fontSize: 15,
+  justifyContent: 'flex-end',
+  alignItems: 'flex-end',
+  textAlign: 'center',
+  backgroundColor: '#fcc181',
+  top: 80,
+  left: 100,
+  marginTop: 10,
+  marginBottom: 0,
+  height: 48,
+  margin: 12,
+  width: 97,
+  borderRadius: 4,
+  padding: 12,
+  shadowColor: "#000",
+  shadowOffset: {
+  width: 0,
+  height: 2,
+  },
+ shadowOpacity: 0.25,
+ shadowRadius: 2,
+}
 });
