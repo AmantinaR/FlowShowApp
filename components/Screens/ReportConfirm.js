a921f21e34b86730f9b703d4a61e5aae21d8b0a9
AmaraO416 <78100470+AmaraO416@users.noreply.github.com>
2022-03-03 23:00:04 -0800

    Update ReportConfirm.js

--- components/Screens/ReportConfirm.js
+++ components/Screens/ReportConfirm.js
@@ -75,33 +75,6 @@ export default function ReportConfirm({ route, navigation }) {
       <Text style = {styles.thank_you}>Thank you for submitting a report! You can check the status of the report under the Report Tab.</Text>
       <Text style = {{margin: '5%'}}>report status bar will be here</Text>
       <StatusBar style="auto" />
-<<<<<<< HEAD
-      <View style={styles.summary_body}>
-        <Text style = {{fontWeight: 'bold', fontSize: 18, marginBottom: '1%'}}>Summary of Report</Text>
-        <View style={styles.summary_line}>
-          <Text style = {{fontWeight: 'bold'}}>Building:  </Text>
-          <Text style = {{fontWeight: 'normal'}}>{params.building}  </Text>
-         </View>
-       <View style={styles.summary_line}>
-          <Text style = {{fontWeight: 'bold'}}>Bathroom #:  </Text>
-          <Text style = {{fontWeight: 'normal'}}>{params.room}  </Text>
-       </View>
-       <View style={styles.summary_line}>
-          <Text style = {{fontWeight: 'bold'}}>Products Requested:  </Text>
-          <Text style = {{fontWeight: 'normal'}}>{productlist}  </Text>
-       </View>
-       <View style={styles.summary_line}>
-          <Text style = {{fontWeight: 'bold'}}>Disposal Options Missing:  </Text>
-          <Text style = {{fontWeight: 'normal'}}>{disposallist}  </Text>
-       </View>
-       <View style={styles.summary_line}>
-          <Text style = {{fontWeight: 'bold'}}>Comment:  </Text>
-          <Text style = {{fontWeight: 'normal'}}>{params.comment}  </Text>
-       </View>
-       <View style={styles.summary_line}>
-          <Text style = {{fontWeight: 'bold'}}>Date Submitted:  </Text>
-          <Text style = {{fontWeight: 'normal'}}>{params.date}  </Text>
-=======
       <View style={styles.summary_body}> 
         <Text style = {{fontWeight: 'bold', fontSize: 18, marginBottom: '1%', fontFamily: 'Helvetica'}}>Summary of Report</Text>
         <View style={styles.summary_line}>
@@ -123,7 +96,6 @@ export default function ReportConfirm({ route, navigation }) {
        <View style={styles.summary_line}>
           <Text style = {{fontWeight: 'bold', fontFamily: 'Helvetica'}}>Date Submitted:  </Text> 
           <Text style = {{fontWeight: 'normal', fontFamily: 'Helvetica'}}>{params.date}  </Text>
->>>>>>> 792ecf20aadba6401978e04db98750670a46eb11
        </View>
       </View>
         <GenericButton text={'Done'} onPress={() => navigation.navigate('ReportLanding')}/>
