548fd40b3dbf8d71f2b88a9c1326af4d92b48dbe
AmaraO416 <78100470+AmaraO416@users.noreply.github.com>
2022-03-07 18:11:03 -0800

    Update DefaultRating.js

--- components/lists/data/DefaultRating.js
+++ components/lists/data/DefaultRating.js
@@ -47,10 +47,100 @@ const DefaultRating = [
   [{
     id: 0,
     date: '3-3-2022',
+    number: 4.5,
+    title: 'Best on Campus',
+    description: 'Awesome bathroom with organic tampons!',
+    user: 'Jordan530'
+
+  },
+  {
+    id: 1,
+    date: '3-2-2022',
+    number: 4,
+    title: 'Great Bathroom',
+    description: 'I go here every day after Math 51! Super accessible.',
+    user: 'CodeLover426'
+
+  },
+  {
+    id: 2,
+    date: '3-1-2022',
+    number: 2.5,
+    title: 'Needs some work',
+    description: 'There is a period product dispenser but they were all out of pads',
+    user: 'Tyler47'
+
+  },
+  {
+    id: 3,
+    date: '2-28-2022',
+    number: 5,
+    title: 'Trans Male Friendly!',
+    description: "Was super relieved to see pads and tampons in the men's bathroom",
+    user: 'DogOwner12'
+
+  },
+  {
+    id: 4,
+    date: '2-27-2022',
+    number: 3.5,
+    title: "It's okay",
+    description: "The stall I used didn't have a bag in the small disposal bin",
+    user: 'Euphoria_fan'
+
+  },],
+  [{
+    id: 0,
+    date: '3-3-2022',
     number: 5,
     title: 'Best on Campus',
     description: 'Clean, free period products, great bathroom!',
-    user: 'Yesenia123'
+    user: 'Student101'
+
+  },
+  {
+    id: 1,
+    date: '3-2-2022',
+    number: 5,
+    title: 'Great Bathroom',
+    description: 'I go here every day after Math 51! Super accessible.',
+    user: 'Euphoria_fan'
+
+  },
+  {
+    id: 2,
+    date: '3-1-2022',
+    number: 2.5,
+    title: 'Needs some work',
+    description: 'There is a period product dispenser but they were all out of pads',
+    user: 'Tyler47'
+
+  },
+  {
+    id: 3,
+    date: '2-28-2022',
+    number: 5,
+    title: 'Trans Male Friendly!',
+    description: "Was super relieved to see pads and tampons in the men's bathroom",
+    user: 'TrainLover23'
+
+  },
+  {
+    id: 4,
+    date: '2-27-2022',
+    number: 3.5,
+    title: "It's okay",
+    description: "The stall I used didn't have a bag in the small disposal bin",
+    user: 'Andy19'
+
+  },],
+  [{
+    id: 0,
+    date: '3-3-2022',
+    number: 5,
+    title: 'Best on Campus',
+    description: 'Clean, free period products, great bathroom!',
+    user: 'TrainLover23'
 
   },
   {
@@ -68,7 +158,7 @@ const DefaultRating = [
     number: 2.5,
     title: 'Needs some work',
     description: 'There is a period product dispenser but they were all out of pads',
-    user: 'Amantina52'
+    user: 'Unicorn91'
 
   },
   {
@@ -77,7 +167,7 @@ const DefaultRating = [
     number: 5,
     title: 'Trans Male Friendly!',
     description: "Was super relieved to see pads and tampons in the men's bathroom",
-    user: 'Student101'
+    user: 'Erica613'
 
   },
   {
@@ -92,136 +182,46 @@ const DefaultRating = [
   [{
     id: 0,
     date: '3-3-2022',
-    number: 5,
-    title: 'Best on Campus',
-    description: 'Clean, free period products, great bathroom!',
-    user: 'Yesenia123'
+    number: 3,
+    title: 'Not Great',
+    description: 'There were in-stall bins, but no products!',
+    user: 'BasketBall123'
 
   },
   {
     id: 1,
     date: '3-2-2022',
-    number: 5,
-    title: 'Great Bathroom',
-    description: 'I go here every day after Math 51! Super accessible.',
-    user: 'Molly365'
+    number: 2,
+    title: 'Not Good Bathroom',
+    description: 'I do not appreciate the lack of period dispensers',
+    user: 'Amara98'
 
   },
   {
     id: 2,
     date: '3-1-2022',
     number: 2.5,
-    title: 'Needs some work',
-    description: 'There is a period product dispenser but they were all out of pads',
+    title: 'Needs work',
+    description: 'Clean bathroom, but no pads',
     user: 'Amantina52'
 
   },
   {
     id: 3,
     date: '2-28-2022',
-    number: 5,
-    title: 'Trans Male Friendly!',
-    description: "Was super relieved to see pads and tampons in the men's bathroom",
-    user: 'Student101'
+    number: 1,
+    title: 'Do Not Use',
+    description: "Was in a pinch and could not rely on this bathroom for products",
+    user: 'CSGirl3'
 
   },
   {
     id: 4,
     date: '2-27-2022',
-    number: 3.5,
-    title: "It's okay",
-    description: "The stall I used didn't have a bag in the small disposal bin",
-    user: 'CSGirl3'
-
-  },],
-  [{
-    id: 0,
-    date: '3-3-2022',
-    number: 5,
-    title: 'Best on Campus',
-    description: 'Clean, free period products, great bathroom!',
-    user: 'Yesenia123'
-
-  },
-  {
-    id: 1,
-    date: '3-2-2022',
-    number: 5,
-    title: 'Great Bathroom',
-    description: 'I go here every day after Math 51! Super accessible.',
-    user: 'Molly365'
-
-  },
-  {
-    id: 2,
-    date: '3-1-2022',
-    number: 2.5,
-    title: 'Needs some work',
-    description: 'There is a period product dispenser but they were all out of pads',
-    user: 'Amantina52'
-
-  },
-  {
-    id: 3,
-    date: '2-28-2022',
-    number: 5,
-    title: 'Trans Male Friendly!',
-    description: "Was super relieved to see pads and tampons in the men's bathroom",
-    user: 'Student101'
-
-  },
-  {
-    id: 4,
-    date: '2-27-2022',
-    number: 3.5,
-    title: "It's okay",
-    description: "The stall I used didn't have a bag in the small disposal bin",
-    user: 'CSGirl3'
-
-  },],
-  [{
-    id: 0,
-    date: '3-3-2022',
-    number: 5,
-    title: 'Best on Campus',
-    description: 'Clean, free period products, great bathroom!',
-    user: 'Yesenia123'
-
-  },
-  {
-    id: 1,
-    date: '3-2-2022',
-    number: 5,
-    title: 'Great Bathroom',
-    description: 'I go here every day after Math 51! Super accessible.',
-    user: 'Molly365'
-
-  },
-  {
-    id: 2,
-    date: '3-1-2022',
-    number: 2.5,
-    title: 'Needs some work',
-    description: 'There is a period product dispenser but they were all out of pads',
-    user: 'Amantina52'
-
-  },
-  {
-    id: 3,
-    date: '2-28-2022',
-    number: 5,
-    title: 'Trans Male Friendly!',
-    description: "Was super relieved to see pads and tampons in the men's bathroom",
-    user: 'Student101'
-
-  },
-  {
-    id: 4,
-    date: '2-27-2022',
-    number: 3.5,
-    title: "It's okay",
-    description: "The stall I used didn't have a bag in the small disposal bin",
-    user: 'CSGirl3'
+    number: 0.5,
+    title: "Terrible",
+    description: "I was out of products and there was nothing to help me out",
+    user: 'Andy 19'
 
   },],
 ];
